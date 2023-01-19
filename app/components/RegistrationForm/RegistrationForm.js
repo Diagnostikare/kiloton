ed674d71e2c61ed2eef487528dff47ceb23ebc6b
lamchang <alanlamchang@gmail.com>
2023-01-19 15:12:20 -0600

    Update RegistrationForm.js

--- app/components/RegistrationForm/RegistrationForm.js
+++ app/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./RegistrationForm.module.scss";
 import componentData from "./registrationForm.json";
 import MaterialField from "../form/MaterialField/MaterialField";

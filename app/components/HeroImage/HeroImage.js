4c533840972f01dcc1e713f3f949fd0a9cb400ec
Axel Tinoco <axeltm8@gmail.com>
2025-01-14 17:41:03 -0600

    Change image hero and move buttons

--- app/components/HeroImage/HeroImage.js
+++ app/components/HeroImage/HeroImage.js
@@ -22,18 +22,18 @@ export default function HeroImage(props) {
           alt="kilotón"
           width={1440}
           height={792}
-          src="/assets/images/landing/hero/kiloton2024.png"
+          src="/assets/images/landing/hero/kiloton2025.png"
         />
         <div className={`container ${styles.heroContent}`}>
           <div className="row d-flex flex-column-reverse flex-md-row">
             <div className="col-12 col-sm-10 col-lg-6">
-              <h1 className={`title bold ${styles.title}`}>
+              <h1 className={`title bold ${styles.title} opacity-0`}>
                 Con kilotón
               </h1>
-              <h1 className={`title bold ${styles.title} mb-4`}>
+              <h1 className={`title bold ${styles.title} mb-4 opacity-0`}>
                 <span className={styles.uderline}>¡gana todo el año!</span>
               </h1>
-              <p className={styles.content}>
+              <p className={`${styles.content} opacity-0`}>
                 Participa a tu ritmo, gana puntos y canjéalos por increíbles premios
               </p>
               <div className={styles.buttons}>
@@ -58,7 +58,7 @@ export default function HeroImage(props) {
               </div>
             </div>
             <div className="col-12 col-sm-10 col-lg-6 d-flex">
-              <div className="d-flex justify-content-end w-100">
+              <div className="d-flex justify-content-end w-100 d-none">
                 <Image
                   priority
                   className={'d-flex'}
